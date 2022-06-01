3845ed9b585a3a376c51d05ca92996e06f33e713
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2022-06-02 00:28:41 +0900

    add problem14

---
+++ src/problem14/p.go
@@ -0,0 +1,26 @@
+package problem13
+
+import "strings"
+
+func longestCommonPrefix(strs []string) string {
+	var ans strings.Builder
+	idx := 0
+
+Loop:
+	for {
+		var c byte
+		for _, s := range strs {
+			if len(s) <= idx {
+				break Loop
+			}
+			if c == 0 {
+				c = s[idx]
+			} else if c != s[idx] {
+				break Loop
+			}
+		}
+		ans.WriteByte(c)
+		idx++
+	}
+	return ans.String()
+}
