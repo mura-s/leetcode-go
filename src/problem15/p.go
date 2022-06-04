1362c8b1e9f4ad3353b7af036b66a89426ba81da
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2022-06-04 13:49:41 +0900

    update p15

--- src/problem15/p.go
+++ src/problem15/p.go
@@ -10,30 +10,23 @@ func threeSum(nums []int) [][]int {
 	if len(nums) < 3 {
 		return [][]int{}
 	}
-	numCntMap := make(map[int]int, len(nums))
-	for _, n := range nums {
-		numCntMap[n]++
-	}
 	ansMap := make(map[cand]bool, 0)
 	for i := 0; i < len(nums); i++ {
+		numCntMap := make(map[int]int, 0)
 		for j := i + 1; j < len(nums); j++ {
-			if nums[i] == nums[j] {
-				if c, ok := numCntMap[nums[i]]; !ok || c < 2 {
-					continue
-				}
-			}
+			numCntMap[nums[j]]++
+		}
+		for j := i + 1; j < len(nums); j++ {
 			target := -nums[i] - nums[j]
 			needCnt := 1
-			if target == nums[i] && target == nums[j] {
-				needCnt = 3
-			} else if target == nums[i] || target == nums[j] {
+			if target == nums[j] {
 				needCnt = 2
 			}
-			if c, ok := numCntMap[target]; ok && c >= needCnt {
+			if cnt, ok := numCntMap[target]; ok && cnt >= needCnt {
 				cs := []int{nums[i], nums[j], target}
 				sort.Ints(cs)
-				can := cand{cs[0], cs[1], cs[2]}
-				ansMap[can] = true
+				c := cand{cs[0], cs[1], cs[2]}
+				ansMap[c] = true
 			}
 		}
 	}
