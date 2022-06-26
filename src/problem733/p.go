0f01a92b2e1f1dddad646b73d41d3fb771e08370
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2022-06-26 13:45:28 +0900

    add p733

---
+++ src/problem733/p.go
@@ -0,0 +1,24 @@
+package problem242
+
+func floodFill(image [][]int, sr int, sc int, color int) [][]int {
+	curColor := image[sr][sc]
+	if curColor == color {
+		return image
+	}
+
+	image[sr][sc] = color
+	if sr-1 >= 0 && image[sr-1][sc] == curColor {
+		floodFill(image, sr-1, sc, color)
+	}
+	if sc-1 >= 0 && image[sr][sc-1] == curColor {
+		floodFill(image, sr, sc-1, color)
+	}
+	if sr+1 < len(image) && image[sr+1][sc] == curColor {
+		floodFill(image, sr+1, sc, color)
+	}
+	if sc+1 < len(image[0]) && image[sr][sc+1] == curColor {
+		floodFill(image, sr, sc+1, color)
+	}
+
+	return image
+}
